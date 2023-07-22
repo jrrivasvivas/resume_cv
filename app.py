01de170b7318e7b6bcb01869f7b9defb932df483
jrrivasvivas <jrrivasvivas@gmail.com>
2023-07-22 14:55:26 -0400

    Draft Portafolio Streamlit with Landing Page

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 import streamlit as st
-from PIL import Image
 
 def render_english():
     # Set dark theme
-    st.markdown(
-        """
+    st.markdown("""
         <style>
         body {
             color: white;
             background-color: #1F2937;
         }
         </style>
-        """,
-        unsafe_allow_html=True,
-    )
+        """, unsafe_allow_html=True)
 
     # Background image
-    background_image = Image.open("background.jpg")
-    st.image(background_image, use_column_width=True)
+    st.image("background.jpg", use_column_width=True)
 
     # Profile photo and description
     col1, col2 = st.beta_columns([1, 4])
     with col1:
-        profile_image = Image.open("profile.jpg")
-        st.image(profile_image, width=150)
+        st.image("profile.jpg", width=150)
     with col2:
         st.title("Your Name")
         st.subheader("Telecommunications Engineer, Data Science")
@@ -33,19 +27,86 @@ def render_english():
     st.subheader("Visualizations")
     col3, col4, col5 = st.beta_columns(3)
     with col3:
-        image1 = Image.open("image1.jpg")
-        st.image(image1, use_column_width=True)
+        st.image("image1.jpg", use_column_width=True)
         st.markdown("[Visualization 1](/page1)")
     with col4:
-        image2 = Image.open("image2.jpg")
-        st.image(image2, use_column_width=True)
+        st.image("image2.jpg", use_column_width=True)
         st.markdown("[Visualization 2](/page2)")
     with col5:
-        image3 = Image.open("image3.jpg")
-        st.image(image3, use_column_width=True)
+        st.image("image3.jpg", use_column_width=True)
         st.markdown("[Visualization 3](/page3)")
 
-# Resto del código para renderizar en otros idiomas
+def render_french():
+    # Set dark theme
+    st.markdown("""
+        <style>
+        body {
+            color: white;
+            background-color: #1F2937;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    # Background image
+    st.image("background.jpg", use_column_width=True)
+
+    # Profile photo and description
+    col1, col2 = st.beta_columns([1, 4])
+    with col1:
+        st.image("profile.jpg", width=150)
+    with col2:
+        st.title("Votre Nom")
+        st.subheader("Ingénieur en Télécommunications, Data Science")
+        st.markdown("[GitHub](https://github.com/votre-github) [LinkedIn](https://linkedin.com/in/votre-linkedin)")
+
+    # Links to other pages
+    st.subheader("Visualisations")
+    col3, col4, col5 = st.beta_columns(3)
+    with col3:
+        st.image("image1.jpg", use_column_width=True)
+        st.markdown("[Visualisation 1](/page1)")
+    with col4:
+        st.image("image2.jpg", use_column_width=True)
+        st.markdown("[Visualisation 2](/page2)")
+    with col5:
+        st.image("image3.jpg", use_column_width=True)
+        st.markdown("[Visualisation 3](/page3)")
+
+def render_spanish():
+    # Set dark theme
+    st.markdown("""
+        <style>
+        body {
+            color: white;
+            background-color: #1F2937;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    # Background image
+    st.image("background.jpg", use_column_width=True)
+
+    # Profile photo and description
+    col1, col2 = st.beta_columns([1, 4])
+    with col1:
+        st.image("profile.jpg", width=150)
+    with col2:
+        st.title("Tu Nombre")
+        st.subheader("Ingeniero de Telecomunicaciones, Data Science")
+        st.markdown("[GitHub](https://github.com/tu-github) [LinkedIn](https://linkedin.com/in/tu-linkedin)")
+
+    # Links to other pages
+    st.subheader("Visualizaciones")
+    col3, col4, col5 = st.beta_columns(3)
+    with col3:
+        st.image("image1.jpg", use_column_width=True)
+        st.markdown("[Visualización 1](/page1)")
+    with col4:
+        st.image("image2.jpg", use_column_width=True)
+        st.markdown("[Visualización 2](/page2)")
+    with col5:
+        st.image("image3.jpg", use_column_width=True)
+        st.markdown("[Visualización 3](/page3)")
 
 # Language selection
 language = st.sidebar.selectbox("Language", ["English", "Français", "Español"])
@@ -53,4 +114,7 @@ language = st.sidebar.selectbox("Language", ["English", "Français", "Español"]
 # Render page based on selected language
 if language == "English":
     render_english()
-# Resto del código para renderizar en otros idiomas
+elif language == "Français":
+    render_french()
+elif language == "Español":
+    render_spanish()
